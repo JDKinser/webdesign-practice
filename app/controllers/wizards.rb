5e46e693ee0e93f8bab4da38975af4aff267edd4
Jesse Kinser <email": null>
2016-06-13 20:33:06 -0700

    add wizard controller frame

---
+++ app/controllers/wizards.rb
@@ -0,0 +1,31 @@
+get '/' do
+  redirect '/wizards'
+end
+
+get '/wizards' do
+
+end
+
+get '/wizards/new' do
+
+end
+
+post '/wizards' do
+
+end
+
+get '/wizards/:id' do
+
+end
+
+get '/wizards/:id/edit' do
+
+end
+
+patch '/wizards/:id' do
+
+end
+
+delete '/wizards/:id' do
+
+end
