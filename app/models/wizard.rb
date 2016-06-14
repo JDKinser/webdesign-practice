a8b33d9fe52992ee865f96492000015b86b858b5
Jesse Kinser <email": null>
2016-06-13 21:42:14 -0700

    added password encrypt and authentication

--- app/models/wizard.rb
+++ app/models/wizard.rb
@@ -1,5 +1,17 @@
 class Wizard < ActiveRecord::Base
+
   validates :first_name, :last_name, :email, :password_digest, presence: true
   validates :email, uniqueness: true
+
+  has_secure_password
+
+  def self.authenticate(email, password)
+    user = User.find_by(email: email)
+    if user && user.authenticate == password
+      user
+    else
+      nil
+    end
+  end
 
 end
